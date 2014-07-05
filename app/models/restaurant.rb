7305f6833692be1cefb8f3ad1aa5f55a3ebe53dc
Christiane Kammerl <christiane@Julius-Kammerls-MacBook-Pro.local>
2014-07-05 15:40:41 -0700

    working: user, restaurant, vote controller and model w/strong params

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ActiveRecord::Base
 	attr_accessible :name, :location, :latitude, :longitude, :address, :phone, :postal_code, :locu_id, :website
 	has_many :menu_items
+  accepts_nested_attributes_for :menu_items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
 
   attr_accessible :age, :email, :gender, :password, :password_confirmation, :zip
-  has_secure_password 
+
   has_many :votes
+  accepts_nested_attributes_for :votes
+
+  has_secure_password
   validates_presence_of :password, on: :create
 
 end
