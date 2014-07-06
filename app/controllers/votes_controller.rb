3403db4ab529b0322c329ad86c4eeab6e45c431a
sebabelmar <si.belmar@gmail.com>
2014-07-05 20:45:30 -0700

    UP and DOWN count is working

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,10 +3,20 @@ class VotesController < ApplicationController
 	def create
     user_id = current_user.id
 
-    vote = Vote.new(menu_item_id: params[:menu_item], user_id: user_id, direction: params[:direction])
-    vote.save
+    if user_id
+      vote = Vote.new(menu_item_id: params[:menu_item_id], user_id: user_id, direction: params[:direction])
+      vote.save
+    end
 
-    redirect_to :back
+    # Put this on a get route and available con the show
+    # current_item = MenuItem.find(params[:menu_item_id])
+    # upvote_count = current_item.votes.where(direction: 'up').count
+    # @downvote_count = current_item.votes.where(direction: 'down').count
+
+    # respond_to do |format|
+    #   msg = { total: upvote_count }
+    #   format.json { render :json => msg }
+    # end
   end
 
 end
