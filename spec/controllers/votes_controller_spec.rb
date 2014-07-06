c53535c7d766f1e6aec9f01fdb5ec1dd97c56786
Christiane Kammerl <christiane@Julius-Kammerls-MacBook-Pro.local>
2014-07-06 12:19:34 -0700

    added specs for restaurant,users, votes controller

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -5,14 +5,26 @@ describe VotesController do
  let(:user) { User.find(vote.user_id) } # user and his votes
  let(:item) { Item.find(vote.item_id) } # item and its votes
 
-  context "create" do
+  context 'create' do
 
-    it "sets the user_id field " do
+    it 'sets the user_id field of the vote' do
       expect(vote.user_id).to eq(user.id)
     end
 
-    it "set the item_id field" do
+    it 'set the item_id field of the vote' do
       expect(vote.item_id).to eq(item.id)
     end
+
+    it 'has a current user' do
+      expect(vote.user_id).to_not be_nil
+    end
+
+# BUGBUG - to be updated
+    # it 'saves a new vote in the database' do
+    #   expect{
+    #     post :create, :vote
+    #   }.to change(Vote, :count).by(1)
+    # end
   end
 end
+
