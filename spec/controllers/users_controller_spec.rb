3e04522a2cf1b9a272e5d1fca6149fe2ef128308
Christiane Kammerl <christiane@Julius-Kammerls-MacBook-Pro.local>
2014-07-05 21:10:07 -0700

    added user_controller_spec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe UsersController do
+  context 'create user' do
+   it "redirects to the root path upon save" do
+    post :create, user: FactoryGirl.attributes_for(:user)
+    expect(response).to redirect_to root_url
+    end
+  end
+
+  it "assigns the requested user to @user" do
+    user = create(:user)
+    get :show, id: user
+    expect(assigns(:user)).to eq user
+  end
+end
+
