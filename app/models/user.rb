37d15a06269e57d58d968438499333c270625800
amdziuk <amdziuk@gmail.com>
2014-07-07 15:25:15 -0700

    Cleaned up user validations and formatted the sign-up page to reflect errors appearing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ActiveRecord::Base
 
   attr_accessible :age, :email, :gender, :password, :password_confirmation, :zip
 
-  validates_presence_of :age, :gender, :zip, on: :create
   has_many :votes
   accepts_nested_attributes_for :votes
 
   has_secure_password
-  validates_presence_of :password, on: :create
-
+  validates_presence_of :password
+  validates_presence_of :email
+  validates_uniqueness_of :email, on: :create
+	validates_format_of :zip, :with => /^\d{5}(-\d{4})?$/, :message => "should be in the form 12345 or 12345-1234"  
+  validates_inclusion_of :gender, :in => %w( male female )
+	validates_inclusion_of :age, :in => 0..120, message: "should be valid numeric age"
 end
 
