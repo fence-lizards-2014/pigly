a2da081e1a5dfa5e6c610c8a22113e11c1db2bd1
Shawn Seibert <shawnseibert@gmail.com>
2014-07-07 20:13:29 -0700

    Changed DB string to varchar with null limitations and added D3 library to vendor JS folder

--- db/migrate/20140705032727_create_votes.rb
+++ db/migrate/20140705032727_create_votes.rb
@@ -1,7 +1,7 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-    	t.string :direction
+    	t.string :direction, limit: 4
       t.belongs_to :item
       t.belongs_to :user
 
