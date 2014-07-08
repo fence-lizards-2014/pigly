9003c1c20ea42ab7b39a9383efff47257f7cf482
amdziuk <amdziuk@gmail.com>
2014-07-07 19:54:09 -0700

    Notifications on front page for successfully signing in, up, and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 		@user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to root_path, notice: "Successfully signed in."
     else
       flash.now.alert = "Invalid email or password"
       render "new"
