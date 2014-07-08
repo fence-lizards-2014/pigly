2d087c75b3625213be0c119bd7489dd5fb9dd166
Shawn Seibert <shawnseibert@gmail.com>
2014-07-07 17:23:57 -0700

    added real restaurant and items to seed as restaurant.first

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,23 @@
 # Item.where(name: nil).destroy_all
 
 # The following is fake data to try out:
+Restaurant.create(name: "Range", location: "San Francisco", latitude: "37.759464", longitude: "-122.421550", address: "842 valencia street", phone: "15.282.8283", postal_code: "94110", website: "www.rangesf.com")
+
+["local king salmon with cilantro rice noodles, squid, pickled vegetables, aioli and chili lime vinaigrette",
+  "olive oil poached california white sea bass with english peas, spring onions, mint and kumquat relish",
+  "braised duck leg with fava beans, dried cherry compote, savory granola and hazelnut milk",
+  "roasted chicken with with caramelized squash, toasted farro, almonds and basil gremolata",
+  "pan seared blade steak with crispy brown rice, blue lake beans, charred scallions and black bean sauce",
+  "fava bean falafel with nardello peppers, smoky eggplant, cucumber yogurt and spiced flatbread",
+  "purée of squash soup with black olive tapenade and chives",
+  "young vegetable salad with red leaf lettuces, fresh goat cheese and champagne vinaigrette",
+  "cucumber and nectarine slaw with basil, poppy seed dressing and toasted pumpkin seeds",
+  "smoked trout rillette with pickled red beets, horseradish cream and rye toast",
+  "chicken liver mousse with a frisee salad",
+  "glazed duck wings with marinated mushrooms, sea beans and spicy mustard",
+  "egg noodle pasta with fava bean pesto and herbed ricotta"
+ ].each { |item| Restaurant.first.items.create(name: item)}
+
 25.times do
   user = User.create!(email: Faker::Internet.email,
     age: rand(18..90),
@@ -28,7 +45,7 @@ end
 
 300.times do
   item = Item.create!(name: Faker::Lorem.word,
-     restaurant_id: rand(1..25))
+     restaurant_id: rand(2..25))
 end
 
 2000.times do
