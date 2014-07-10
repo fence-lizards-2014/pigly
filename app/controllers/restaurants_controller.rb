0ca942e22011a39e5be2745a06993804c84eda8e
amdziuk <amdziuk@gmail.com>
2014-07-09 21:42:54 -0700

    Above 90% coverage

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,20 +7,12 @@ class RestaurantsController < ApplicationController
 
 	def show
 		@restaurant = Restaurant.find(params[:id])
-		@items = sort_by_percent(@restaurant)
+		@items = @restaurant.sort_by_percent
 		@top_five_items = @items.shift(5) # lists top 5 menu items with content
 		@remaining_items = @items
 		@nodes_for_bubbles = AgeBubbleController.new((params[:id])).get_nodes_for_restaurant_id.to_json
 		@visnodes = QueryController.item_nodes(@restaurant).nodes_to_json
 		@visedges = QueryController.item_edges(@restaurant).edges_to_json
-	end
-
-	private
-
-	def sort_by_percent(restaurant)
-		@voted_items = restaurant.items.select {|item| item.votes.count > 0}
-		@unvoted_items = restaurant.items.select {|item| item.votes.count == 0}
-		@voted_items.sort_by! { |item| item.calculate_percentage }.reverse + @unvoted_items
 	end
 
 end
--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -2,9 +2,4 @@ module AuthenticationHelpers
   def stub_current_user user
     ApplicationController.any_instance.stub(:current_user) { user }
   end
-
-  def stub_authorize_user!
-    ApplicationController.any_instance.stub(:authorize_user!) { true }
-  end
-
 end
